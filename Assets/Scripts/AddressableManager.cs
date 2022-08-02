9930aded95487edbc72fbbff3bd0a6c62f72ddf5
F.M. Panggabean <cichiomaru@users.noreply.github.com>
2022-08-02 20:19:12 +0700

    add log to debug.log

--- Assets/Scripts/AddressableManager.cs
+++ Assets/Scripts/AddressableManager.cs
@@ -81,6 +81,7 @@ public class AddressableManager : MonoBehaviour
     private void Log(string value)
     {
         log.text += $"[{DateTime.Now.ToString("hh:mm:ss:ff")}] {value}\n";
+        Debug.Log($"[{DateTime.Now.ToString("hh:mm:ss:ff")}] {value}\n");
     }
 
     private void Message(string value)
